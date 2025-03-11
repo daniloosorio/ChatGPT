83e043879da09a452c13c63de2fa80a3b66e4e93
Danilo Osorio <odanilo38@gmail.com>
2025-03-10 21:36:51 -0500

    change key

--- ChatGPT/ContentView.swift
+++ ChatGPT/ContentView.swift
@@ -5,7 +5,7 @@
 //  Created by Danilo Osorio on 10/03/25.
 //
 
-//API KEY CHAT GPT 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
+// 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
 
 import SwiftUI
 
