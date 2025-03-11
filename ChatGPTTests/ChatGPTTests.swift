d90f80fd51dabb17b2ec8364b65c1995f72ece6f
Danilo Osorio <odanilo38@gmail.com>
2025-03-10 21:22:20 -0500

    Initial Commit

---
+++ ChatGPTTests/ChatGPTTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTTests.swift
+//  ChatGPTTests
+//
+//  Created by Danilo Osorio on 10/03/25.
+//
+
+import Testing
+@testable import ChatGPT
+
+struct ChatGPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
