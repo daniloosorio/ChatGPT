28a6f5606ff7f54cf12558ff9522acee550253f0
Danilo Osorio <odanilo38@gmail.com>
2025-03-17 22:06:05 -0500

    app chat gpt done

--- ChatGPT/Presentation/Edit/EditView.swift
+++ ChatGPT/Presentation/Edit/EditView.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import HelpersTutorialDALLE2
 
 struct EditView: View {
     @StateObject var viewModel = EditViewModel()
     @State var text: String = ""
     @State var selectedImage: Image?
     @State var emptyImage: Image = Image(systemName: "photo.on.rectangle.angled")
+    
+    @State var showCamera: Bool = false
+    @State var showGallery: Bool = false
+    
+    @State var lines: [Line] = []
+    @FocusState var isFocused: Bool
     
     var currentImage: some View {
         if let selectedImage {
@@ -27,8 +34,115 @@ struct EditView: View {
         }
     }
     var body: some View {
-        Text("Create a mask")
-            .font(.headline)
-            .padding(.vertical, 12)
+        Form {
+            Text("Create a mask")
+                .font(.headline)
+                .padding(.vertical, 12)
+            
+            AsyncImage(url: viewModel.imageURL){ image in
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .overlay(alignment:.bottomTrailing) {
+                        Button {
+                            Task{
+                                await viewModel.saveImageGalery()
+                            }
+                        } label: {
+                            HStack {
+                                Image(systemName: "arrow.down.circle.fill")
+                                    .resizable()
+                                    .frame(width: 30,height: 30)
+                                    .shadow(color: .black, radius: 0.2)
+                            }
+                            .padding(8)
+                            .foregroundColor(.green)
+                        }
+                    }
+            }
+            placeholder: {
+                VStack{
+                    if !viewModel.isLoading {
+                        ZStack{
+                            currentImage
+                            SwiftBetaCanvas(lines:$lines,currentLineWidth: 30)
+                        }
+                    } else{
+                        ProgressView()
+                            .padding(.bottom,12)
+                        Text("Pintando imagen 🌆")
+                            .multilineTextAlignment(.center)
+                    }
+                }
+                .frame(width: 300,height: 300)
+            }
+            HStack{
+                Button{
+                    showCamera.toggle()
+                }label:{
+                    Text("📸 take photo")
+                }.tint(.orange)
+                    .buttonStyle(.borderedProminent)
+                    .fullScreenCover(isPresented: $showCamera) {CameraView(selectedImage: $selectedImage)}
+                    .padding(.vertical,12)
+                
+                Spacer()
+                Button{
+                    showGallery.toggle()
+                }label:{
+                    Text("🖼️ open gallery")
+                }.tint(.orange)
+                    .buttonStyle(.borderedProminent)
+                    .fullScreenCover(isPresented: $showGallery) {GalleryView(selectedImage: $selectedImage)}
+                    .padding(.vertical,12)
+            }
+            
+            TextField("Que Quieres editar!!",
+                      text:$text,
+                      axis: .vertical)
+            .lineLimit(10)
+            .lineSpacing(5)
+            .focused($isFocused)
+            .onSubmit {
+                    isFocused = false // Cierra el teclado
+                }
+            
+            HStack{
+                Spacer()
+                Button("Generar Imagen"){
+                    isFocused = false
+                    
+                    let selectedImageRender = ImageRenderer(content: currentImage)
+                    let maskRenderer = ImageRenderer(content: currentImage.reverseMask({
+                        SwiftBetaCanvas(lines: $lines,currentLineWidth: 30)
+                    }))
+                    
+                    Task {
+                        guard let selecteduiImage = selectedImageRender.uiImage,
+                              let selectedPNGData = selecteduiImage.pngData(),
+                              let maskuiImage = maskRenderer.uiImage,
+                              let maskPNG = maskuiImage.pngData() else {
+                            return
+                        }
+                        Task{
+                          await  self.viewModel.generateEdit(withText: text, imageData: selectedPNGData, maskData: maskPNG)
+                        }
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .disabled(viewModel.isLoading)
+                
+                Button("Reset"){
+                    viewModel.imageURL = nil
+                    selectedImage = nil
+                    lines.removeAll()
+                }
+                .tint(.red)
+                .buttonStyle(.borderedProminent)
+                .disabled(viewModel.isLoading)
+            }
+            .padding(.vertical,12)
+        }
     }
+        
 }
--- ChatGPT/Presentation/Edit/EditViewModel.swift
+++ ChatGPT/Presentation/Edit/EditViewModel.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 @MainActor
 final class EditViewModel: ObservableObject {
     private let urlSession: URLSession
     @Published var imageURL: URL?
     @Published var isLoading = false
+    private let apiKey: String = "apikey"
     
     init(urlSession: URLSession = URLSession.shared) {
         self.urlSession = urlSession
@@ -26,7 +28,7 @@ final class EditViewModel: ObservableObject {
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "POST"
         urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.addValue("Bearer apikey", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
         
         let dictionary: [String: Any] = [
             "n": 1,
@@ -76,6 +78,42 @@ final class EditViewModel: ObservableObject {
         }
     }
 
+    func generateEdit(withText text: String,imageData: Data, maskData: Data) async {
+        let url = URL(string: "https://api.openai.com/v1/images/edits")!
+        
+        let headers = HTTPHeaders(["Authorization":"Bearer \(apiKey)"])
+        
+        let dictionary = [
+            "n":"1",
+            "size":"1024x1024",
+            "prompt": text
+        ]
+        
+        DispatchQueue.main.async{
+            self.isLoading = true
+        }
+        
+        AF.upload(multipartFormData: { multipartFormData in
+            for (key,value) in dictionary {
+                if let data = value.data(using: .utf8){
+                    multipartFormData.append(data,withName: key)
+                }
+            }
+            multipartFormData.append(imageData, withName: "image",fileName: "image.png", mimeType: "image/png")
+            multipartFormData.append(maskData, withName: "mask",fileName: "mask.png", mimeType: "image/png")
+        }, to: url, headers: headers).responseDecodable(of: ModelResponse.self) { dataResponse in
+            let model = try! JSONDecoder().decode(ModelResponse.self, from: dataResponse.data!)
+            
+            DispatchQueue.main.async {
+                self.isLoading = false
+                guard let firstModel = model.data.first else {
+                    return
+                }
+                self.imageURL = URL(string: firstModel.url)!
+                print(self.imageURL ?? "No imageURL")
+            }
+        }
+    }
     
 }
 
